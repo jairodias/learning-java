ab24aa2bae8b63a43c2f195aec347212fd39686d
Jairo Dias <jairopereira_dias@hotmail.com>
2021-07-25 13:43:00 -0300

    refactor: implementing calculator class with static methods

--- curso_programacao/src/exercises/application/ExerciseFour.java
+++ curso_programacao/src/exercises/application/ExerciseFour.java
@@ -3,10 +3,9 @@ package exercises.application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import exercises.util.Calculator;
+
 public class ExerciseFour {
-
-    public static final double PI = 3.14159;
-
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner income = new Scanner(System.in);
@@ -14,17 +13,10 @@ public class ExerciseFour {
         System.out.println("Enter radius: ");
         double radius = income.nextDouble();
 
-        System.out.printf("Circumference: %.2f%n", circumference(radius));
-        System.out.printf("Volume: %.2f%n", volume(radius));
-        System.out.printf("PI value: %.2f%n", PI);
+        System.out.printf("Circumference: %.2f%n", Calculator.circumference(radius));
+        System.out.printf("Volume: %.2f%n", Calculator.volume(radius));
+        System.out.printf("PI value: %.2f%n", Calculator.PI);
+
         income.close();
-    }
-
-    public static double circumference(double radius) {
-        return 2.0 * PI / radius;
-    }
-
-    public static double volume(double radius) {
-        return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
